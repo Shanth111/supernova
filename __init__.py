df121ae398158a60b6b95096b35affb300a21e66
ketanpujare <ketanpujare@gmail.com>
2018-03-28 00:01:55 +0530

    review connection to mysql

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template,request,url_for,redirect
 #from opinion import opinion
 from dbconn import connection
+from MySQLdb import escape_string as thwart
+import gc
+
 
 app = Flask(__name__)
 
@@ -10,8 +13,22 @@ app = Flask(__name__)
 def homepage():
     return render_template('main.html')
 
-@app.route('/review')
+@app.route('/review', methods=['GET','POST'])
 def review():
+    if request.method == 'POST':
+        website = request.form['website']
+        user_review = request.form['user_review']
+        c, conn = connection()
+
+        c.execute("INSERT INTO review (webname,review) VALUES(%s,%s)", (thwart(website),thwart(user_review)))
+        conn.commit()
+
+        c.close()
+        conn.close()
+        gc.collect()
+        
+        return render_template('main.html')
+        
     return render_template('review.html')
 
     
