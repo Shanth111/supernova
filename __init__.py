10694b5db24980407254d2a1d34dbf5781689547
ketanpujare <ketanpujare@gmail.com>
2018-04-01 20:03:42 +0530

    result page added

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from flask import Flask,render_template,request,url_for,redirect
-#from opinion import opinion
-from dbconn import connection
 from MySQLdb import escape_string as thwart
+from dbconn import connection
+from textblob import TextBlob
+#from opinion import opinion
 import gc
 
 
@@ -13,20 +14,29 @@ app = Flask(__name__)
 def homepage():
     if request.method == 'POST':
         search = request.form['search']
-        print(search)
-        
+
         c, conn = connection()
         c.execute("SELECT review FROM review WHERE webname='"+search+"'")
         data = c.fetchall()
-        print(data)
         #conn.commit()
 
         c.close()
         conn.close()
         gc.collect()
-        
-        return render_template('review.html')
+# Other File
+        pol = 0
+        for rd in data:
+            blob = TextBlob(str(rd))
+            pol = pol + blob.sentiment.polarity*100
+            print(pol)
+# Other File 
+        return redirect(url_for('result',search=search,pol=pol))
     return render_template('main.html')
+
+@app.route('/result/<search>/<pol>/',methods=['GET','POST'])
+def result(search,pol):
+    return render_template('result.html',search=search,pol=pol)
+
 
 @app.route('/review', methods=['GET','POST'])
 def review():
@@ -42,7 +52,7 @@ def review():
         conn.close()
         gc.collect()
         
-        return render_template('main.html')
+        return render_template('review.html')
         
     return render_template('review.html')
 
