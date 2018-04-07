38c1ae30796c906d911dce1854af5c7cb8d7b543
ketanpujare <ketanpujare@gmail.com>
2018-04-08 01:17:49 +0530

    data_scrap_3

--- data.py
+++ data.py
@@ -5,23 +5,23 @@ import requests
 page = requests.get('https://www.mouthshut.com/online-shopping')
 tree = html.fromstring(page.content)
 
-# sites = []
-# links = []
-links = ['https://www.mouthshut.com/product-reviews/Gingercrush-com-reviews-925899269']
-# with open('webrate.csv', 'wb') as csvfile:
-#     csv = writer(csvfile)
-#     for i in range(1,42):
-#         website = tree.xpath('//*[@id="categorierightpanel"]/div/div[3]/div[1]/div[%d]/div[2]/div[1]/a/text()'%i)
-#         weblink = tree.xpath('//*[@id="categorierightpanel"]/div/div[3]/div[1]/div[%d]/div[2]/div[1]/a/@href'%i)
-#         no_review = tree.xpath('//*[@id="categorierightpanel"]/div/div[3]/div[1]/div[%d]/div[2]/div[2]/div[3]/a/text()'%i)
-#         try:
-#             data = [website[0],weblink[0],no_review[0]]
-#             sites.append(website[0])
-#             links.append(weblink[0])
-#         except IndexError:
-#             continue
+sites = []
+links = []
 
-#         csv.writerow(data)
+with open('webrate.csv', 'wb') as csvfile:
+    csv = writer(csvfile)
+    for i in range(1,42):
+        website = tree.xpath('//*[@id="categorierightpanel"]/div/div[3]/div[1]/div[%d]/div[2]/div[1]/a/text()'%i)
+        weblink = tree.xpath('//*[@id="categorierightpanel"]/div/div[3]/div[1]/div[%d]/div[2]/div[1]/a/@href'%i)
+        no_review = tree.xpath('//*[@id="categorierightpanel"]/div/div[3]/div[1]/div[%d]/div[2]/div[2]/div[3]/a/text()'%i)
+        try:
+            data = [website[0],weblink[0],no_review[0]]
+            sites.append(website[0])
+            links.append(weblink[0])
+        except IndexError:
+            continue
+
+        csv.writerow(data)
 
 
 
@@ -57,25 +57,24 @@ with open('review.csv', 'wb') as reviewfile:
         site_name = tree.xpath('//*[@id="prodTitle1"]/a/text()')
         page = tree.xpath('//*[@id="spnPaging"]/li[last()]/a/text()')
 
-
-        # for k in range(20):
-        #     #review_name = tree.xpath('//*[@id="ctl00_ctl00_ContentPlaceHolderFooter_ContentPlaceHolderBody_rptreviews_ctl%02d_divProfile"]/p[1]/a/text()'%k)
-        #     review_title = tree.xpath('//*[@id="ctl00_ctl00_ContentPlaceHolderFooter_ContentPlaceHolderBody_rptreviews_ctl%02d_lnkTitle"]/text()'%k)
-        #     review = tree.xpath('//*[@id="ctl00_ctl00_ContentPlaceHolderFooter_ContentPlaceHolderBody_rptreviews_ctl%02d_lireviewdetails"]/div[2]/text()'%k)
+        for k in range(20):
+            #review_name = tree.xpath('//*[@id="ctl00_ctl00_ContentPlaceHolderFooter_ContentPlaceHolderBody_rptreviews_ctl%02d_divProfile"]/p[1]/a/text()'%k)
+            review_title = tree.xpath('//*[@id="ctl00_ctl00_ContentPlaceHolderFooter_ContentPlaceHolderBody_rptreviews_ctl%02d_lnkTitle"]/text()'%k)
+            review = tree.xpath('//*[@id="ctl00_ctl00_ContentPlaceHolderFooter_ContentPlaceHolderBody_rptreviews_ctl%02d_lireviewdetails"]/div[2]/text()'%k)
             
             
-        #     #print(page)
-        #     print(site_name)
-        #     # #print(review_name)
-        #     # print(review_title)
-        #     # print(review)
+            #print(page)
+            print(site_name)
+            # #print(review_name)
+            # print(review_title)
+            # print(review)
 
-        #     try:
-        #         review_data = [site_name[0],review_title[0].encode('utf-8'),review[0].encode('utf-8')]
-        #     except IndexError:
-        #         continue
+            try:
+                review_data = [site_name[0],review_title[0].encode('utf-8'),review[0].encode('utf-8')]
+            except IndexError:
+                continue
 
-        #     csv.writerow(review_data)
+            csv.writerow(review_data)
         
         
         # for k in range(2,int(page[0])+1):
--- postd.py
+++ postd.py
@@ -1,6 +1,8 @@
 from lxml import html
 import requests
 
+links = ['https://www.mouthshut.com/product-reviews/Gingercrush-com-reviews-925899269']
+
 
 payload ={'catid':'925899269','catname':'Gingercrush.com','corp':'false','fbmessage':'I+found+this+review+of+Gingercrush.com+pretty+useful','isvideo':'false','prodimg':'.jpg','rating_str':'+1/5','reviewid':'2531646','twitterlnk':'https://www.mouthshut.com/review/Gingercrush-com-review-nmnorprmoqn','twittermsg':'I+found+this+review+of+Gingercrush.com+pretty+useful+%23WriteShareWin','type':'review','usession':'0'}
 r = requests.post("https://www.mouthshut.com/review/CorporateResponse.ashx", data=payload)
